7013b9e992c4f484594834d8fdc70cee57858438
1spyral <83098914+1spyral@users.noreply.github.com>
2022-01-13 18:55:31 -0500

    Implemented new features
    - Changed window size
    - Added methods to statemachine

--- Const.java
+++ Const.java
@@ -1,6 +1,6 @@
 public class Const {
-    public static final int WIDTH = 800;
-    public static final int HEIGHT = 600;
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 1024;
     public static final int FRAME_PERIOD = 10;
     
     public static final int BALL_RADIUS = 10;
--- StateMachine.java
+++ StateMachine.java
@@ -5,20 +5,60 @@ public class StateMachine implements Drawable {
     StartMenu startMenu;
     GameMenu gameMenu;
     PauseMenu pauseMenu;
+    SelectMenu selectMenu;
 
     boolean start;
     boolean game;
     boolean pause;
+    boolean select;
 
+    public Lambda toggleStart;
+    public Lambda toggleGame;
+    public Lambda togglePause;
+    public Lambda toggleSelect;
     StateMachine() {
+        startMenu = new StartMenu(this);
+        gameMenu = new GameMenu(this);
+        pauseMenu = new PauseMenu(this);
+        selectMenu = new SelectMenu(this);
+
         start = true;
         game = false;
         pause = false;
+        select = false;
+
+        toggleStart = () -> start = !start;
+        toggleGame = () -> game = !game;
+        togglePause = () -> pause = !pause;
+        toggleSelect = () -> select = !select;
     }
     public void draw(Graphics g) {
-        
+        if (start) {
+            startMenu.draw(g);
+        }
+        else if (game) {
+            gameMenu.draw(g);
+        }
+        else if (pause) {
+            gameMenu.draw(g);
+            pauseMenu.draw(g);
+        }
+        else if (select) {
+            selectMenu.draw(g);
+        }
     }
     public void update(Set<Integer> keysPressed, boolean mouseDown, int mouseX, int mouseY) {
-
+        if (start) {
+            startMenu.update(keysPressed, mouseDown, mouseX, mouseY);
+        }
+        else if (game) {
+            gameMenu.update(keysPressed, mouseDown, mouseX, mouseY);
+        }
+        else if (pause) {
+            pauseMenu.update(keysPressed, mouseDown, mouseX, mouseY);
+        }
+        else if (select) {
+            selectMenu.update(keysPressed, mouseDown, mouseX, mouseY);
+        }
     }
 }
